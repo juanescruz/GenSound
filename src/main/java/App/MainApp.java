e6eda95f7209fd807ba77237067ff922cde12627
Ana María <anicu2314@gmail.com>
2024-05-11 20:00:29 -0500

    Canciones mostradas en inicio usuario

--- src/main/java/App/MainApp.java
+++ src/main/java/App/MainApp.java
@@ -17,7 +17,7 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        FXMLLoader loader = new FXMLLoader( MainApp.class.getResource("/View/inicioPrincipal.fxml") );
+        FXMLLoader loader = new FXMLLoader( MainApp.class.getResource("/View/inicioSesion.fxml") );
         Parent parent = loader.load();
         Scene scene = new Scene(parent);
         stage.setScene(scene);
